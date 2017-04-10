65a8e8650b2f143b1fcbc6a44c886c82dd2f3634
hugo-clems <hugo.clems@icloud.com>
2017-04-10 14:03:24 +0200

    Mise au propre du code v2

--- src/gen.ml
+++ src/gen.ml
@@ -4,24 +4,61 @@ open Lang
 open Analyses
 open Instrs
 
-(* ************************************************************ *)
-(* **** Compilation of expressions / statements            **** *)
-(* ************************************************************ *)
 
+
+
+
+
+
+
+(* ************************************************* *)
+(* *** 2. Typage et compilation pour expressions *** *)
+(* ************************************************* *)
+
+(* *** 2.3 - Génération de code pour expressions *** *)
+(* Corresponds aux exercices 3, 4 & 5 *)
+
+
+
+
+(* *** Gestion des erreurs *** *)
 
 (* Si un élément ne se trouve pas dans la liste *)
 exception PasDansLaliste;;
 
 
-(* Détermine la position d'un élément dans la liste (à partir de 0) *)
+(* Supprime les warnings lors de la compilation, code temporaire *)
+exception CodeAVenirDansLaPartieII;;
+
+
+
+
+(* *** Fonction auxiliaire *** *)
+
+
+(**
+ * Détermine la position d'un élément dans une liste
+ * @param elt - l'élément recherché
+ * @param la liste où l'on cherche
+ * @return l'indice de position de l'élément (à partir de 0)
+ * @throw PasDansLaliste - si l'élément ne se trouve pas dans la liste
+ *)
 let rec position = function elt -> function
 	| [] -> raise PasDansLaliste
 	| (a::reste) -> if a = elt then 0 else 1 + position elt reste;;
 
 
-exception CodeAVenirDansLaPartieII;;
 
-(* Transforme l'expression en ByteCode *)
+
+(* *** Fonction principale *** *)
+
+(**
+ * Traduit une expression en instruction
+ * @param variables - liste des variables de la fonction actuelle
+ * @param l'expression à traduire
+ * @return l'instruction traduite
+ * @throw CodeAVenirDansLaPartieII - TEMPORAIRE
+ *)
 let rec gen_expr = function variables -> function
 	| Const (cType, cValeur) -> [Loadc (cType, cValeur)]
 	| VarE (vType, Var (varBinding, vVname)) -> [Loadv (vType, (position vVname variables))]
@@ -30,6 +67,10 @@ let rec gen_expr = function variables -> function
 			e1 @ e2 @ [Bininst (bType, bBinop)]
 	| IfThenElse (iType, iexpr1, iexpr2, iexpr3) -> raise CodeAVenirDansLaPartieII
 	| CallE (ceType, ceNom, ceArgs) -> raise CodeAVenirDansLaPartieII;;
+
+
+
+
 
 
 
